157dddd654a4df9d85d65b486b782b8fa12a404d
Abhishek2401 <56971447+Abhishek2401@users.noreply.github.com>
2019-10-25 18:39:04 +0530

    Upate the code

--- stack.cpp
+++ stack.cpp
@@ -19,144 +19,65 @@
 // THE SOFTWARE.
 
 #include <iostream>
-using namespace std;
-
-// Defining the maximum size of the stack to be 10
-#define MAX 10
-
-// Defining a class to store the member variables
-// and member functions required
-class Stack {
-  int stk[MAX], top;
-
-public:
-  Stack(): top(-1) {}
-  void push(int );
-  int pop();
-  void display();
-};
-
-
-/*
-
-ALGORITHM FOR PUSHING AN ELEMENT IN THE STACK:
-
-  1) IF TOP = MAX-1
-        WRITE Overflow
-        EXIT
-
-  2) TOP = TOP + 1
-  3) STACK[TOP] = VALUE
-  4) EXIT
-
-*/
-
-void Stack :: push(int value) {
-  // When the size of stack has reached maximum,
-  // we cannot insert any element
-  if(top == MAX-1) {
-    cout<<"The Stack is full !\n";
-  }
-  else {
-    // Incrementing top
-    top++;
-
-    // Store the value in the stack at top position
-    stk[top] = value;
-  }
+using namespace std; 
+int stack[100], n=100, top=-1;
+void push(int val) {
+   if(top>=n-1)
+      cout<<"Stack Overflow"<<endl; 
+   else {
+      top++;
+      stack[top]=val;
+   }
 }
-
-
-/*
-
-ALGORITHM FOR POPPING AN ELEMENT FROM THE STACK:
-
-  1) IF TOP = -1
-        WRITE Underflow
-        RETURN -1
-        EXIT
-
-  2) VALUE = STK[TOP]
-  3) TOP = TOP - 1
-  4) RETURN VALUE
-  5) EXIT
-
-*/
-
-int Stack :: pop() {
-  // When the size of stack is -1, the stack is empty
-  if(top == -1) {
-    cout<<"The Stack is empty !\n";
-    return -1;
-  }
-  else {
-    // Store the top value
-    int value = stk[top];
-
-    // Decrement top
-    top --;
-
-    return value;
-  }
+void pop() {
+   if(top<=-1)
+      cout<<"Stack Underflow"<<endl;
+   else {
+      cout<<"The popped element is "<< stack[top] <<endl;
+      top--;
+   }
 }
-
-
-// Display the elements of the Stack by decrementing starting from top
-void Stack :: display() {
-  // When the size of stack is -1, the stack is empty
-  if(top == -1) {
-    cout<<"The Stack is empty !\n";
-  }
-  else {
-    // Decrementing stack starting from top position
-    for(int i=top; i>=0; i--) {
-      // Printing each stack element
-      cout<<stk[i]<<" ";
-    } cout<<endl;
-  }
+void display() {
+   if(top>=0) {
+      cout<<"Stack elements are:";
+      for(int i=top; i>=0; i--)
+         cout<<stack[i]<<" ";
+         cout<<endl;
+   } else
+      cout<<"Stack is empty";
 }
-
-
-// Main Function
 int main() {
-  int option, value;
-  Stack s;
-
-  do {
-    cout<<"\n**** MENU ****\n"
-        <<"1. Push\n"
-        <<"2. Pop\n"
-        <<"3. Display\n"
-        <<"4. Exit\n";
-
-    cout<<"Enter your option: ";
-    cin>>option;
-
-    switch(option) {
-      case 1: cout<<"Enter the value to be pushed: ";
-              cin>>value;
-
-              s.push(value);
-              break;
-
-      case 2: value = s.pop();
-              if(value != -1) {
-                cout<<"The popped element is: "<<value<<endl;
-              }
-              break;
-
-      case 3: s.display();
-              break;
-
-      case 4: break;
-
-      default: cout<<"Wrong option !\n";
-               break;
-    }
-  } while(option != 4);
-
-  if(option == 4) {
-    cout<< "\nTHANK YOU for using the program !\n"
-        <<"Have a good day.\n\n";
-  }
+   int ch, val; 
+   cout<<"1) Push in stack"<<endl;
+   cout<<"2) Pop from stack"<<endl;
+   cout<<"3) Display stack"<<endl;
+   cout<<"4) Exit"<<endl;
+   do {
+      cout<<"Enter choice: "<<endl;
+      cin>>ch;
+      switch(ch) {
+         case 1: {   
+            cout<<"Enter value to be pushed:"<<endl;
+            cin>>val;
+            push(val);
+            break;
+         }
+         case 2: {
+            pop();
+            break;
+         }
+         case 3: {
+            display();
+            break;
+         }
+         case 4: {
+            cout<<"Exit"<<endl;
+            break;
+         }
+         default: {
+            cout<<"Invalid Choice"<<endl;
+         }
+      }
+   }while(ch!=4); 
+      return 0;
 }
